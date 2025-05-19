9df88693fdef09279895ba648d13ac934c8c17ef
jeisyane <160151590+jeisyane@users.noreply.github.com>
2025-05-19 12:12:22 -0300

    Add files via upload

---
+++ exercicicio1.py
@@ -0,0 +1,7 @@
+def soma(a ,b):
+    return a + b
+
+a=2
+b=3
+
+print("a soma e ",soma(a ,b))
---
+++ exercicio2.py
@@ -0,0 +1,11 @@
+def par_ou_impar(n):
+
+    if n%2==0:
+        return "par"
+
+    else:
+        return "impar"
+
+n = 5
+
+print("o numero é:", par_ou_impar(n))
